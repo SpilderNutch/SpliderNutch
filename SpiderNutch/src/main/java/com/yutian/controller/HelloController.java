f4fd030c965d57faa225065f275b975ad11a5dac
RockyOu0102 <wakemen106@163.com>
2016-01-20 19:18:05 +0800

    1，一个非常简陋的项目。结合了Spring MVC/Mybatis/Msql 的WEB项目。

--- SpiderNutch/src/main/java/com/yutian/controller/HelloController.java
+++ SpiderNutch/src/main/java/com/yutian/controller/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.google.common.base.Stopwatch;
 import com.yutian.model.UserInfo;
 import com.yutian.service.UserInfoService;
 
@@ -21,11 +22,17 @@ public class HelloController {
 	
 	@RequestMapping(value="hello")
 	public String hello(Model model){
+		Stopwatch sw = Stopwatch.createStarted();
+		
 		
 		logger.info("HelloController hello method had invoked!");
 		
 		UserInfo userinfo = userInfoService.getUserInfoById("111");
 		System.out.println(userinfo);
+		
+		sw.stop();
+		logger.info("hello method :"+sw.toString()); 
+		
 		return "index.jsp";
 	}
 
